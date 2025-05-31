a60abb3544873ed5c74a77f215f20071c396c27f
smuhammadkahaf <smuhammadkahaf@gmail.com>
2025-06-01 00:09:46 +0500

    delete

--- 1. VOSYS/BaseClass.py
+++
@@ -1,9 +0,0 @@
-from DataBase import Database
-
-class BaseClass:
-    def __init__(self):
-        self.db = None
-
-    def ensure_db(self):
-        if not self.db:
-            self.db = Database
